f48d7bc5141467848827b2f79c534fd9a7f61c8b
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-07-14 23:32:13 +0200

    Chat fully responsive real time with action cable and jobs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,7 @@ class HomeController < ApplicationController
     session[:conversations] ||= []
 
     @users = User.all.where.not(id: current_user)
-    @conversations = policy_scope(Conversation).includes(:recipient, :messages)
-                                 .find(session[:conversations])
+    @conversations = policy_scope(Conversation).includes(:recipient, :messages).find(session[:conversations])
 
   end
 end
