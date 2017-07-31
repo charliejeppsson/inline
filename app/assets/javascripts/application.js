f40a50b62d79ca6feb4c6441a428b2ae1b9da660
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-07-31 02:10:13 +0200

    make chat (home#index) mobile-responsive

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,7 @@
     var messages_list = panel.find('.messages-list');
 
     panel.find('.panel-body').toggle();
-    panel.attr('class', 'panel');
+    panel.attr('class', 'panel my-panel');
 
     if (panel.find('.panel-body').is(':visible')) {
       var height = messages_list[0].scrollHeight;
